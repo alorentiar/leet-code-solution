afb65ee3af5cd1a2090b471ced6464e6255068c3
Alfredo Lorentiar Santonanda <alorentiar03@gmail.com>
2023-08-03 23:53:03 +0700

    adding question

--- LengthOfLastWord.py
+++ LengthOfLastWord.py
@@ -1,5 +1,6 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        # Question : https://leetcode.com/problems/length-of-last-word/?envType=study-plan-v2&envId=top-interview-150
         #Separate words by space
         lastword = list(s.split())
 
